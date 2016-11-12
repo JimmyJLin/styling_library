9a278369033fa10147eefd07af81b807bd94778a
Jimmy Lin <usmcjimmy@gmail.com>
2016-11-11 21:56:25 -0500

    added buttons

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,25 @@
 $(function(){
   console.log("running script file")
-  
+
+  // Toggle Buttons - add class active if clicked and remove if click again
+  $('.button.toggle').on("click", function(){
+    if($(this).hasClass('selected')){
+      $(this).removeClass('selected').text('Vote')
+    } else {
+      $(this).addClass('selected').text('Voted')
+    }
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
